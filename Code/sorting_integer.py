24ab6834c15006d94781c48c7124245675257c32
siko408 <gamerbloxhd@gmail.com>
2020-04-15 16:34:41 -0700

    Sorting finished

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,4 +1,6 @@
 #!python
+from sorting_iterative import insertion_sort
+from linkedList import LinkedList
 
 
 def counting_sort(numbers):
@@ -11,6 +13,34 @@ def counting_sort(numbers):
     # TODO: Loop over given numbers and increment each number's count
     # TODO: Loop over counts and append that many numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+    # find max and min
+    min, max = numbers[0], numbers[0]
+    for index in range(len(numbers)):
+        if numbers[index] > max:
+            max = numbers[index]
+        if min > numbers[index]:
+            min = numbers[index]
+    #  create temporary list
+    temp_list = []
+    for _ in range(max-min):
+        temp_list.append(0)
+    #  Fill temp list
+    for index in range(len(numbers)):
+        if numbers[index] == max:
+            temp_list[len(temp_list)-1] += 1
+            continue
+        temp_list[numbers[index]-min] += 1
+    numbers.clear()
+    extend = 0
+    for index in range(len(temp_list)):
+        for _ in range(temp_list[index]):
+            if index + 1 == max-min:
+                #  We are going to add max value
+                numbers.insert(index + extend, (min + index) + 1)
+            else:
+                numbers.insert(index + extend, min + index)  # index = numbers[value] - min
+            extend += 1
+    print(numbers)
 
 
 def bucket_sort(numbers, num_buckets=10):
@@ -24,3 +54,80 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+    load_factor = 0.75  # This will make the bucket sort dynamic.
+    # if num_buckets is None:
+    #     num_buckets = 1  # Default
+
+    buckets = []  # Each bucket will contain a linked List
+    for _ in range(num_buckets):
+        buckets.append(LinkedList())
+    min, max = numbers[0], numbers[0]
+    for index in range(len(numbers)):
+        if numbers[index] > max:
+            max = numbers[index]
+        if min > numbers[index]:
+            min = numbers[index]
+
+    for index in range(len(numbers)):
+        linkedList_index = (numbers[index] * len(buckets)-1)//max
+        sort_index = indexSort(buckets[linkedList_index].items(), numbers[index])
+        if sort_index is None:
+            buckets[linkedList_index].append(numbers[index])
+        else:
+            buckets[linkedList_index].insert_at_index(sort_index, numbers[index])
+
+    number_index = 0
+    for index in range(len(buckets)):
+        bucket = buckets[index].items()
+        for linkedIndex in range(len(bucket)):
+            numbers[number_index] = bucket[linkedIndex]
+            number_index += 1
+    return numbers
+
+        #  Insertion sort the linked list
+
+
+def indexSort(list, value):
+    """ Find index of where the value should be inserted"""
+    index = 0
+    if len(list) == 0:
+        return 0
+    if len(list) == 1:
+        if list[0] > value:
+            # list.insert(0, value)
+            return 0
+        else:
+            # list.append(value)
+            return None
+    insertion = False
+    for index in range(len(list)-1):
+        if list[index] < value and value < list[index+1]:
+            # list.insert(index + 1, value)
+            index = index+1
+            insertion = True
+            break
+    if insertion:
+        return index
+    else:
+        if list[0] > value:
+            # list.insert(0, value)
+            return 0
+        else:
+            # list.append(value)
+            return None
+
+
+if __name__ == "__main__":
+    list = [1, 5, 10, 2, 2, 3, 4, 6, 7, 20, 20, 100, 100, 3, 10]
+    list2 = [0, 1, 2, 10, 12, 15, 20]
+    counting_sort(list)
+    print(bucket_sort(list))
+    #  Find index
+    # value = 4
+    # print(indexSort(list2, value))
+    # index = indexSort(list2, value)
+    # if index is None:
+    #     list2.append(value)
+    # else:
+    #     list2.insert(indexSort(list2, value), value)
+    # print(list2)
--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -44,7 +44,11 @@ def split_sort_merge(items):
     # TODO: Merge sorted halves into one list in sorted order
     list = insertion_sort(items)
     index = int(len(list)/2)
-    return list[:index], list[index:]
+    first, second = list[:index], list[index:]
+    result = merge(first, second)
+    items[:] = result
+    return
+    # return list[:index], list[index:]
 
 
 def merge_sort(items):
@@ -65,7 +69,7 @@ def merge_sort(items):
         result = merge(first_half, second_half)
         for index in range(len(items)):
             items[index] = result[index]
-        print(items)
+    return items
 
 
 def partition(items, low, high):
@@ -75,11 +79,54 @@ def partition(items, low, high):
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+
+    # Pivot will start in the middle, between low and high given points
     # TODO: Choose a pivot any way and document your method in docstring above
     # TODO: Loop through all items in range [low...high]
     # TODO: Move items less than pivot into front of range [low...p-1]
     # TODO: Move items greater than pivot into back of range [p+1...high]
     # TODO: Move pivot item into final position [p] and return index p
+    pivot_point = ((high)+low)//2
+    print("Pivot point", pivot_point, "low", low, "high", high)
+    pivot = items[pivot_point]  # start in the middle
+    low_index, high_index = low, high
+    dict_index = {"low": [],
+                  "high": [],
+                  "list": []
+                  }
+    while low_index < pivot_point and high_index > pivot_point:
+        if items[low_index] > pivot:
+            dict_index["low"].append(low_index)
+        else:
+            dict_index["list"].append(("low", low_index))
+        print("High index", high_index, items)
+        if items[high_index] < pivot:
+            dict_index["high"].append(high_index)
+        else:
+            dict_index["list"].append(("high", high_index))
+        low_index += 1
+        high_index -= 1
+    #  Add pivot to the last in low section
+    #  Change later
+    temp_list = []
+    dict_index['list'].insert(0, ('high', pivot_point))
+    for index in range(len(dict_index['list'])):
+        if dict_index['list'][index][0] == 'low':
+            temp_list.insert(0, items[dict_index['list'][index][1]])
+        else:
+            temp_list.append(items[dict_index['list'][index][1]])
+    for lower in range(len(dict_index['low'])):
+        temp_list.append(items[dict_index['low'][lower]])
+        if pivot_point > 0:
+            pivot_point -= 1
+    for higher in range(len(dict_index['high'])):
+        temp_list.insert(0, items[dict_index['high'][higher]])
+        pivot_point += 1
+    print("Items", items, "temp", temp_list)
+    items.clear()
+    for final_index in range(len(temp_list)):
+        items.append(temp_list[final_index])
+    return pivot_point
 
 
 def quick_sort(items, low=None, high=None):
@@ -92,8 +139,24 @@ def quick_sort(items, low=None, high=None):
     # TODO: Check if list or range is so small it's already sorted (base case)
     # TODO: Partition items in-place around a pivot and get index of pivot
     # TODO: Sort each sublist range by recursively calling quick sort
+    # Check if high and low range bounds have default values (not given)
+    if low is None and high is None:
+        low, high = 0, len(items) - 1
+
+    # Check if list or range is so small it's already sorted (base case)
+    if high > 1:
+        # Partition items in-place around a pivot and get index of pivot
+        # O(n): iterates the entire list
+        partitioned_loc = partition(items, low, high)
+        # Sort each sublist range by recursively calling quick sort
+        quick_sort(items, low, partitioned_loc)
+        print("Partition", partitioned_loc, items)
+        quick_sort(items, partitioned_loc + 1, high-1)
+    return
 
 
 if __name__ == "__main__":
     #  Merge
-    merge_sort([3, 15, 4, 7, 20, 6, 18, 11, 9, 7])
+    print(merge_sort([3, 15, 4, 7, 20, 6, 18, 11, 9, 7]))
+    list = [41, 3, 15, 4, 1, 20, 51, 18, 11, 9, 7]
+    quick_sort(list)
